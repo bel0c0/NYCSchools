e512ddde346e2c10c792a76f60e2a2e511a7325c
Ben Law <ben@blbusiness.com>
2022-05-07 21:29:22 -0400

    Initial Commit

---
+++ 20220508-BenjaminLaw-NYCSchools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20220508-BenjaminLaw-NYCSchools
+//
+//  Created by Benjamin Law on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20220508-BenjaminLaw-NYCSchools/_0220508_BenjaminLaw_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0220508_BenjaminLaw_NYCSchoolsApp.swift
+//  20220508-BenjaminLaw-NYCSchools
+//
+//  Created by Benjamin Law on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct _0220508_BenjaminLaw_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
